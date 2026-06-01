09e60be0fc23dcdfc9612a657fdd72f6d1284238
Kavin <kavins030609@gmail.com>
2026-06-01 08:17:16 -0400

    ksjd

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import useApi from './useApi'
-
+let d = import.meta.env.VITE_SDATA || 'nodata'
 export default function Home() {
+  console.log( import.meta.url.length)
   let [n] = useApi()
-  console.log(n);
-  let d = import.meta.SDATA
   return (
     <>
       {d}
